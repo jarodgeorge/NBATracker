e61e091242b9d961e9fb3e0e14b6acf6397b0354
Jarod <jaroddanielgeorge@gmail.com>
2021-08-25 23:25:41 -0400

    updates

--- Backend/db.js
+++ Backend/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "***REMOVED***",
+    password: "",
     host: "localhost",
     port: 5432,
     database: "nba_alerts"
